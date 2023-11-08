22cef4392fed90b6451bff3b44c2d7e1f2aac71f
unknown <meghg2021@gmail.com>
2023-11-08 10:27:37 -0500

    First change

---
+++ src/Counter.js
@@ -0,0 +1,22 @@
+
+import {useState, useEffect} from "react";
+
+function Counter(){
+    const [count, setCount] = useState(0);
+    const [calculation, setCalculation] = useState(0);
+
+
+     useEffect(() =>{ 
+        setCalculation(()=> count*2)}, [count]);
+
+
+    return(
+        <>
+        <p>Count: {count}</p>
+        <button onClick = {() => setCount((count)=>(count + 1))}>Click me</button>
+        <p>Calculation:{calculation}</p>
+        </>
+    )
+}
+
+export default Counter;
